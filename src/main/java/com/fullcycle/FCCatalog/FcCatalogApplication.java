923bec02ca81ecbd1ae1efc5af44c28e88843e14
eduardo3g <eduardosbrasil10@gmail.com>
2021-10-06 16:22:07 -0300

    initial commit

---
+++ src/main/java/com/fullcycle/FCCatalog/FcCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.fullcycle.FCCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullcycle/FCCatalog/FcCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullcycle.FCCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
